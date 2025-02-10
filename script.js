1d62619e54a4ae6c5b715983fb2784326d39697b
Jordan-182 <Jordan.P62@outlook.fr>
2025-02-10 19:52:37 +0100

    init + hero section

---
+++ script.js
@@ -0,0 +1,11 @@
+const wordLetters = document.querySelectorAll(".word") || [];
+wordLetters.forEach((el) => {
+  const chars = [...el.textContent];
+  el.innerHTML = "";
+  chars.forEach((char, index) => {
+    const span = document.createElement("span");
+    span.textContent = char;
+    span.style.setProperty("--index", index);
+    el.append(span);
+  });
+});
