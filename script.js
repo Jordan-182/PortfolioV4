7298c00646ac1f60f35ae22080a0b2f068f9ac35
Jordan-182 <Jordan.P62@outlook.fr>
2025-02-12 19:32:32 +0100

    12.02.25

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-const wordLetters = document.querySelectorAll(".word") || [];
-wordLetters.forEach((el) => {
-  const chars = [...el.textContent];
-  el.innerHTML = "";
-  chars.forEach((char, index) => {
-    const span = document.createElement("span");
-    span.textContent = char;
-    span.style.setProperty("--index", index);
-    el.append(span);
+const splitting = () => {
+  document.querySelectorAll(".word").forEach((word) => {
+    const letters = word.innerText.split("");
+    word.innerHTML = null;
+    letters.forEach((letter) => {
+      word.innerHTML += `<span class="letter">${letter}</span>`;
+    });
+  });
+};
+splitting();
+
+document.querySelectorAll(".letter").forEach((span, index) => {
+  span.style.setProperty("--i", index);
+});
+
+document.querySelectorAll(".link").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const targetId = link.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        left: 0,
+        behavior: "smooth",
+      });
+    }
   });
 });
